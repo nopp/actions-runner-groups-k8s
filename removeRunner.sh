72ad9e821e180e1ff302928d3f1c5b5281ecdc52
Carlos Augusto Malucelli <camalucelli@gmail.com>
2024-01-10 12:26:23 -0300

    fix removeRunner.sh

--- removeRunner.sh
+++ removeRunner.sh
@@ -3,8 +3,6 @@
 # Generate Runner token
 export RUNNER_TOKEN=$(curl -s -X POST https://api.github.com/orgs/${ORGANIZATION_NAME}/actions/runners/registration-token -H "accept: application/vnd.github.everest-preview+json" -H "authorization: token ${GITHUB_PAT}" | jq -r '.token')
 
-cd /opt
-
 # Remove the runner
-echo "Removing runner."
+cd /opt
 sudo -E -u sre ./config.sh remove --token ${RUNNER_TOKEN}
